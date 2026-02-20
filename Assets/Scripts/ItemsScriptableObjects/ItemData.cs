6a219e5a487902c5ba23613aa445f402d7e8eed8
ropuerta <rpuertais@edu.tecnocampus.cat>
2026-02-20 14:09:19 +0100

    Scriptable Objects and Localization
    creation scripts scriptable objects and localization for taking damage

---
+++ Assets/Scripts/ItemsScriptableObjects/ItemData.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Items/ItemData")]
+public class ItemData : ScriptableObject
+{
+    [Header("Details")]
+    public string Name;
+    public string Description;
+
+
+    public bool IsConsumable;
+    [Header("If consumable is true")]
+    public int LifeRestore;
+
+    [Header("Economy")]
+    public int Cost;
+    public int Sell;
+
+    [Header("Image")]
+    public Sprite Image;
+}
